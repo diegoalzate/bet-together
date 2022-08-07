40fc741bb2aff383e0cfce978f88f12b5a274f8a
bfranceschin <bfranceschin@gmail.com>
2022-08-07 14:12:27 -0300

    feat: token mint test

--- packages/backend/test/index.ts
+++ packages/backend/test/index.ts
@@ -53,4 +53,28 @@ describe("BettingPool", function () {
     const open = await poolContract.openForBets();
     expect(open).to.equal(true); // open
   })
+
+  // TODO: move mint test to different test script
+  it("Token mint test", async () => {
+    const amount = 10;
+    const address = accounts[0].address;
+    const tx = await tokenContract.mint(address, amount);
+    await tx.wait();
+    const balance = await tokenContract.balanceOf(address);
+    expect(balance).to.equal(amount); // open
+  })
+
+  // it("Simple pool test", async () => {
+  //   // mint players tokens
+  //   const initialTokenBalances = [10, 20, 30, 40];
+  //   // console.log(accounts[0]);
+  //   const tx = await tokenContract.mint(accounts[0], initialTokenBalances[0]);
+  //   // for (let i = 0; i<initialTokenBalances.length; ++i) {
+  //   //   const tx = await tokenContract.mint(accounts[i], initialTokenBalances[i]);
+  //   //   await tx.wait();
+  //   // }
+  //   // for (let i = 0; i<initialTokenBalances.length; ++i) {
+
+  //   // }
+  // })
 });
