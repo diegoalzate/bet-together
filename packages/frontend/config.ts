156a10f7682db3b3562d227cc572d3509ff372e4
Diego Alzate <diego35.da@gmail.com>
2022-08-09 22:07:16 -0500

    feat: connect to betting factory

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -1,2 +1,4 @@
-export const NETWORK_ID = 31337 as number;
-export const NETWORK_NAME = 'localhost' as string;
+export const NETWORK_ID = 80001 as number;
+export const NETWORK_NAME = "mumbai" as string;
+export const EIGHTEENZERO = 10 ** 18;
+export const USDC_TESTNETMINTABLE_POLYGON = "0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2"
--- packages/frontend/pages/pool/index.tsx
+++ packages/frontend/pages/pool/index.tsx
@@ -1,6 +1,42 @@
+/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
+import { useContractRead, useContractWrite } from "wagmi";
+import { NETWORK_ID, USDC_TESTNETMINTABLE_POLYGON } from "@/config";
+import contracts from "@/contracts/hardhat_contracts.json";
 
+type Pool = {
+  address: string;
+  game: string;
+  yieldTime: string;
+  status: boolean;
+  amount: number;
+  coin: string;
+};
+const chainId = Number(NETWORK_ID);
+const allContracts = contracts as any;
+console.log(allContracts[chainId]);
+const bettingPoolFactoryAddress =
+  allContracts[chainId][0].contracts.BettingPoolFactory.address;
+const bettingPoolFactoryABI =
+  allContracts[chainId][0].contracts.BettingPoolFactory.abi;
 const Pool = () => {
+  const { data: poolCount, isLoading: poolCountIsLoading } = useContractRead({
+    addressOrName: bettingPoolFactoryAddress,
+    contractInterface: bettingPoolFactoryABI,
+    functionName: "poolCount",
+  });
+  const {
+    data: createdPool,
+    write,
+    isSuccess,
+    isLoading: createIsLoading,
+  } = useContractWrite({
+    addressOrName: bettingPoolFactoryAddress,
+    contractInterface: bettingPoolFactoryABI,
+    functionName: "createDefaultPool",
+    args: [USDC_TESTNETMINTABLE_POLYGON],
+  });
+  console.log(poolCount?.toString());
   const bets = [
     {
       game: "Coin Flip",
@@ -27,7 +63,17 @@ const Pool = () => {
   return (
     <>
       <div className="flex flex-col space-y-8">
-        <h2 className="text-5xl font-bold text-white">Betting pools</h2>
+        <div className="flex justify-between">
+          <h2 className="text-5xl font-bold text-white">Betting pools</h2>
+          <button
+            className="btn bg-pBlue text-white"
+            onClick={() => {
+              write();
+            }}
+          >
+            create pool
+          </button>
+        </div>
         <div className="overflow-x-auto">
           <table className="table w-full">
             <thead>
@@ -39,21 +85,41 @@ const Pool = () => {
               </tr>
             </thead>
             <tbody>
-              {bets.map((item, i) => (
-                <Link href={`/pool/${i}`} key={i}>
-                  <tr className="hover">
-                    <th>{item.game}</th>
-                    <td>{item.yieldTime}</td>
-                    <td>{item.status ? "Open" : "Closed"}</td>
-                    <td>{`${item.amount} ${item.coin}`}</td>
-                  </tr>
-                </Link>
-              ))}
+              {[...Array.from({ length: +(poolCount?.toString() ?? 0) })].map(
+                (item, i) => (
+                  <PoolRow key={i} index={i} />
+                )
+              )}
             </tbody>
           </table>
         </div>
       </div>
     </>
+  );
+};
+
+const PoolRow = (props: { index: number }) => {
+  const {
+    data: pool,
+    isError,
+    isLoading: poolsIsLoading,
+  } = useContractRead({
+    addressOrName: bettingPoolFactoryAddress,
+    contractInterface: bettingPoolFactoryABI,
+    functionName: "pools",
+    args: [props.index],
+  });
+  console.log(pool);
+  return (
+    <Link href={`/pool/${pool?.address}`}>
+      <tr className="hover">
+        {pool?.address}
+        {/* <th>{item.game}</th>
+                    <td>{item.yieldTime}</td>
+                    <td>{item.status ? "Open" : "Closed"}</td>
+                    <td>{`${item.amount} ${item.coin}`}</td> */}
+      </tr>
+    </Link>
   );
 };
 
