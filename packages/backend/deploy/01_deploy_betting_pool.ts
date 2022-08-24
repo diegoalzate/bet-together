abe24b64d99150922dcb1c096c24c3182998bfaf
bfranceschin <bfranceschin@gmail.com>
2022-08-23 22:38:00 -0300

    feat: goerli deploy adjustment

--- packages/backend/deploy/01_deploy_betting_pool.ts
+++ packages/backend/deploy/01_deploy_betting_pool.ts
@@ -1,12 +1,29 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
-const main: DeployFunction = async function ({getNamedAccounts, deployments}: HardhatRuntimeEnvironment) {
+const main: DeployFunction = async function ({getNamedAccounts, deployments, ethers, getChainId}: HardhatRuntimeEnvironment) {
+  console.log("Deploying the betting pool");
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  // const chainId = await getChainId();
+  // if (chainId !== '80001') {
+  //   return;
+  // }
+
+  let token: string | undefined;
+
+  const chainId = await getChainId();
+  switch (chainId) {
+    case '80001': { // polygon mumbai
+      token = "0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2"
+    }
+    case '5': { // goerli
+      token = "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43"; // USDC-TestnetMintableERC20-Aave
+    }
+  }
+
   const random = "0xB087848Ca9565995105a8aE803B33719bD395a7C"
-  const token = "0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2"
   const args = [random, token, random, random];
   await deploy('BettingPool', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
